66e01189036fb69551658bae1f00b8679bf51801
Matvei Kaptiug <matvei_1906@mail.ru>
2023-07-07 15:33:43 +0300

    first

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import { createSiteNavigationTemplate } from './view/site-navigation.js';
+import { createTripInfoTemplate } from './view/trip-info.js';
+import { createTripCostTemplate } from './view/trip-cost.js';
+import { createFiltersTemplate } from './view/filter.js';
+import { createTripBordTemplate } from './view/trip-bord.js';
+import { createNewPointTemplate } from './view/new-point.js';
+import { createEditPointTemplate } from './view/edit-point.js';
+import { createPointTemplate } from './view/point.js';
+
+const POINT_COUNT = 3;
+
+const render = (container, template, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const navigationMenuContainer = document.querySelector('.trip-controls__navigation');
+render(navigationMenuContainer, createSiteNavigationTemplate());
+
+const tripMain = document.querySelector('.trip-main');
+render(tripMain, createTripInfoTemplate(), 'afterbegin');
+
+const tripMainInfo = document.querySelector('.trip-info');
+render(tripMainInfo, createTripCostTemplate());
+
+const filtersContainer = document.querySelector('.trip-controls__filters');
+render(filtersContainer, createFiltersTemplate());
+
+const eventsContainer = document.querySelector('.trip-events');
+render(eventsContainer, createTripBordTemplate());
+
+const eventsList = document.querySelector('.trip-events__list');
+render(eventsList, createEditPointTemplate());
+render(eventsList, createNewPointTemplate());
+
+for (let i = 0; i < POINT_COUNT; i++) {
+  render(eventsList, createPointTemplate());
+}
